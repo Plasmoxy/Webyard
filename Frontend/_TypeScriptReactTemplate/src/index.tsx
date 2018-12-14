349161663c3d6ef6e0d53348673351ba480fd35a
Plasmoxy <sebohacker@gmail.com>
2018-12-14 18:22:10 +0100

    template

---
+++ Frontend/_TypeScriptReactTemplate/src/index.tsx
@@ -0,0 +1,18 @@
+// react
+import * as React from 'react'
+import { render } from 'react-dom'
+import '../src/style.scss'
+
+interface AppProps {
+    
+}
+
+export const App = (p: AppProps) => <div>
+
+</div>
+
+
+render(
+    <App />,
+    document.getElementById('app'),
+)
