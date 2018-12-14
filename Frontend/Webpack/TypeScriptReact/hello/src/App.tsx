1f0d1376804f331b530b7b63499123d2ef317f67
Plasmoxy <sebohacker@gmail.com>
2018-12-14 15:25:58 +0100

    xdd

--- Frontend/Webpack/TypeScriptReact/hello/src/App.tsx
+++ Frontend/Webpack/TypeScriptReact/hello/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Button, Row, Col } from 'reactstrap'
+import { add } from './counter'
 
 export const Greeter = (p: {name: string}) => (
     <div className="text-warning">
@@ -21,8 +22,14 @@ export class IncreasingButton extends React.Component {
         this.count += 1
         this.forceUpdate()
     }
-
 }
+
+
+export const PersonGreeter = (props: {name: string, age: number}) => (
+    <h1 className="text-danger">
+        Hello, I'm {props.name} and I'm {props.age} years old.
+    </h1>
+)
 
 export default () => <div className="container">
     <h1> Greeter </h1>
@@ -35,4 +42,18 @@ export default () => <div className="container">
         <Col><IncreasingButton /></Col>
         <Col><IncreasingButton /></Col>
     </Row>
+
+    <PersonGreeter name="Bobi" age={19} />
+    <PersonGreeter name="Seb" age={17} />
+    <PersonGreeter name="Corentin" age={25} />
+
+    <div className="text-boom">
+    we're in slovakia and we're pdartying
+    </div>
+
+    {add(1, 2)}
+
+
+
+
 </div>
