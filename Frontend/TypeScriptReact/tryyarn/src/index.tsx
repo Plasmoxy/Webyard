1f5d19a35959543a5b6acce799750cbfc7c4e0d3
Plasmoxy <sebohacker@gmail.com>
2018-12-14 20:08:23 +0100

    legitgoodyarn

--- Frontend/TypeScriptReact/tryyarn/src/index.tsx
+++ Frontend/TypeScriptReact/tryyarn/src/index.tsx
@@ -3,16 +3,12 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import '../src/style.scss'
 
-import { Button } from 'reactstrap'
-
 interface AppProps {
     
 }
 
 export const App = (p: AppProps) => <div className="container">
-    <h1 className="text-warning">
-        Hello !!
-    </h1>
+
 </div>
 
 
