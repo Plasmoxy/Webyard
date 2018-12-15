de2491c8a1c30afb0d005ef870b22bfdc3286646
Plasmoxy <sebohacker@gmail.com>
2018-12-15 10:54:53 +0100

    babelstuff

---
+++ Frontend/_BabelReactTemplate/src/index.tsx
@@ -0,0 +1,18 @@
+// react
+import * as React from 'react'
+import { render } from 'react-dom'
+import '../src/style.scss'
+
+interface AppProps {
+    
+}
+
+export const App = (p: AppProps) => <div className="container mt-3">
+
+</div>
+
+
+render(
+    <App />,
+    document.getElementById('app'),
+)
