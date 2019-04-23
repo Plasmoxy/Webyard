65a5850117cfb756b9c4f18b2e76e67e0da7f6b8
Plasmoxy <sebohacker@gmail.com>
2019-04-23 11:18:30 +0200

    yaeet

---
+++ clean.js
@@ -0,0 +1,23 @@
+const glob = require("glob") // for finding files/directories
+const rimraf = require("rimraf") // for removing recursively
+
+function clean(folder, target) {
+    if (!folder || folder == "") {
+        console.log("ERROR: No target folder found. Specify the target folder as argument!")
+        return
+    }
+    
+    const match = folder + "/**/" + target
+    console.log("[Removing all files by query : " + match + " ]")
+    
+    glob(match, function (er, files) {
+        for (let file of files) {
+            console.log("Removing " + file)
+            rimraf.sync(file)
+        }
+    })
+}
+
+// clean nodemodules in specified folder
+clean(process.argv[2], "node_modules/")
+
