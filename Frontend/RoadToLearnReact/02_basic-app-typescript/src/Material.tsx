40c1491fb1fda63d7545d8e8399845b08a99b0b3
Plasmoxy <sebohacker@gmail.com>
2019-04-24 21:43:50 +0200

    Update Material.tsx

--- Frontend/RoadToLearnReact/02_basic-app-typescript/src/Material.tsx
+++ Frontend/RoadToLearnReact/02_basic-app-typescript/src/Material.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-export default class extends React.Component {
-    constructor(props: any) {
+interface Props {}
+
+interface State {
+    count: number
+}
+
+export default class extends React.Component<Props, State> {
+    
+    constructor(props: Props) {
         super(props)
+        this.state = {
+            count: 0
+        }
     }
 
     render() {
-        return <Button variant="contained" color="primary">X asD</Button>
+        return <div>
+            <Button variant="contained" color="primary" onClick={this.btnClick.bind(this)}>Count: {this.state.count}</Button>
+        </div>
+    }
+
+    btnClick() {
+        this.setState((state) => ({
+            count: this.state.count + 1
+        }))
     }
 }
