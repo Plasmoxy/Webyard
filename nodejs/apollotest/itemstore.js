f581d01e1c64defbb87d6c3e4017c2c6a0e7a369
Plasmoxy <sebohacker@gmail.com>
2019-07-09 08:41:53 +0200

    ts lol

---
+++ nodejs/apollotest/itemstore.js
@@ -0,0 +1,25 @@
+const { promisify } = require("util")
+const fs = require("fs")
+const bettersqlite = require("better-sqlite3")
+const { ApolloServer, gql } = require('apollo-server')
+
+const db = bettersqlite("./sqlite.db")
+
+const resolvers = ({
+    Query: {
+        allItems: () => db.prepare("select * from items").all(),
+        item: (p, a) => db.prepare("select * from items where id=?").get(a.id)
+    },
+    Mutation: {
+        createItem: (parent, args) => {
+            let info = db.prepare("insert into items (name, price) values (?, ?)").run(args.name, args.price)
+            return info.lastInsertRowid
+        }
+    }
+})
+
+const server = new ApolloServer({ typeDefs: gql(fs.readFileSync("schema.gql", "utf-8")), resolvers });
+
+server.listen().then(({ url }) => {
+    console.log(`Graphql sql 🚀  Server ready at ${url}`);
+});
