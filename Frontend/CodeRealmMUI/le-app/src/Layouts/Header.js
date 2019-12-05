a34c38e8205cf0c0df3bdf3cce2630a8c8fa5cc2
Plasmoxy <sebohacker@gmail.com>
2019-12-05 21:58:43 +0100

    weird errors

---
+++ Frontend/CodeRealmMUI/le-app/src/Layouts/Header.js
@@ -0,0 +1,46 @@
+import React from "react"
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from "@material-ui/core"
+import { MenuIcon } from "@material-ui/icons/Menu"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
+
+export default props => {
+  const classes = useStyles()
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
