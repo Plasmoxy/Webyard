50de5678d0690c583ed0a9ad2eec018ecd9daf5d
Plasmoxy <sebohacker@gmail.com>
2019-12-06 19:24:29 +0100

    Update main.js

--- Frontend/CodeRealmMUI/le-simple/public/main.js
+++ Frontend/CodeRealmMUI/le-simple/public/main.js
@@ -7,7 +7,6 @@ function createWindow () {
     let win = new BrowserWindow({width: 800, height: 600}) 
 
     win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
-    win.webContents.openDevTools()
 }
        
 // and load the index.html of the app.     win.loadFile('index.html')   }      
