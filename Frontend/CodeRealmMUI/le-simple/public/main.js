df4b8d10e3e9c1191d661ee754d1d0144b952ba9
Plasmoxy <sebohacker@gmail.com>
2019-12-06 19:00:39 +0100

    yy

--- Frontend/CodeRealmMUI/le-simple/public/main.js
+++ Frontend/CodeRealmMUI/le-simple/public/main.js
@@ -1,11 +1,12 @@
-const {app, BrowserWindow} = require('electron')  
+const {app, BrowserWindow} = require('electron')
+const path = require("path")
 
 
 function createWindow () {   
     // Create the browser window.     
     let win = new BrowserWindow({width: 800, height: 600}) 
 
-    win.loadURL("http://localhost:3000")
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
     win.webContents.openDevTools()
 }
        
