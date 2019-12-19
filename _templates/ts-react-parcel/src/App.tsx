116e9e72ced57944f82ec8a1e3bf8fafed9907a4
Plasmoxy <sebohacker@gmail.com>
2019-12-19 20:31:16 +0100

    template fôr ts react

---
+++ _templates/ts-react-parcel/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { render } from "react-dom"
+import './style.sass'
+
+function App() {
+  return <>
+    
+  </>
+}
+
+render(<App />, document.getElementById("reactRoot"))
