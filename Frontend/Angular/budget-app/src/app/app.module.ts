caa1688662ab41b67b4c057e3250eca62fc4c4a7
Plasmoxy <sebohacker@gmail.com>
2020-08-09 14:05:28 +0200

    nice

--- Frontend/Angular/budget-app/src/app/app.module.ts
+++ Frontend/Angular/budget-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    BudgetItemListComponent
   ],
   imports: [
     BrowserModule,
