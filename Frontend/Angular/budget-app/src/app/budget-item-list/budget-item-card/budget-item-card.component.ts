9a4c06a4bc69f83c578ab3401ee9b8fc1b7053a8
Plasmoxy <sebohacker@gmail.com>
2020-08-09 15:53:53 +0200

    ok binding works

--- Frontend/Angular/budget-app/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ Frontend/Angular/budget-app/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BudgetItem } from 'src/models/budget-item.model'
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
   
-  @Input() isIncome: boolean = true;
+  @Input() item: BudgetItem;
   
   constructor() { }
 
