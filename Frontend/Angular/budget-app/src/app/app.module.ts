4fd3beccd5bc8cb36ab57eebb9ea12c5260b3294
Plasmoxy <sebohacker@gmail.com>
2020-08-09 14:09:27 +0200

    yes

--- Frontend/Angular/budget-app/src/app/app.module.ts
+++ Frontend/Angular/budget-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
 import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     AddItemFormComponent,
-    BudgetItemListComponent
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
