a29da4eea301d5b6299ebb1617b5eeb70eb5f505
Plasmoxy <sebohacker@gmail.com>
2020-08-14 13:25:11 +0200

    atlast

--- BartZadanie/pmxy-gallery/src/App.tsx
+++ BartZadanie/pmxy-gallery/src/App.tsx
@@ -3,14 +3,59 @@ import 'regenerator-runtime/runtime'
 import React from "react"
 import { render } from "react-dom"
 import './App.scss'
+import './images/gallery-thumb.jpg';
 import { Container } from 'react-bootstrap'
 import { Switch } from 'react-router-dom'
 
 function App() {
   return <>
-    <Container className="app-content">
+    <div className="background-container">
+      <img src="images/nightcity.jpg" />
+    </div>
+    
+    <div className="container content">
+      <h1 className="text-light">FOTOGALÉRIA</h1>
+      <h2 className="text-light mt-5">KATEGÓRIE</h2>
+      <hr />
+      <div className="mt-4"></div>
       
-    </Container>
+      <div className="gallery-tiles">
+        <div className="gallery-row">
+          <div className="gallery-column">
+            <div className="gallery-card">
+              <img src="images/town.jpg" />
+              <div className="p-1 pt-2 text-center text-uppercase">
+                <span>Hory</span>
+              </div>
+            </div>
+          </div>
+          <div className="gallery-column">
+            <div className="gallery-card">
+              <img src="images/walkercity.jpg" />
+              <div className="p-1 pt-2 text-center text-uppercase">
+                <span>Mestá</span>
+              </div>
+            </div>
+          </div>
+          <div className="gallery-column">
+            <div className="gallery-card">
+              <img src="images/gallery-thumb.jpg" />
+              <div className="p-1 pt-2 text-center text-uppercase">
+                <span>Hory</span>
+              </div>
+            </div>
+          </div>
+          <div className="gallery-column">
+            <div className="gallery-card">
+              <img src="./images/gallery-thumb.jpg" />
+              <div className="p-1 pt-2 text-center text-uppercase">
+                <span>Hory</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
   </>
 }
 
