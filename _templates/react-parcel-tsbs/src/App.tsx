514b34145fb4896049588d0cd790b12e5b552904
Plasmoxy <sebohacker@gmail.com>
2020-08-14 11:25:41 +0200

    this gettin good

--- _templates/react-parcel-tsbs/src/App.tsx
+++ _templates/react-parcel-tsbs/src/App.tsx
@@ -2,11 +2,14 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import React from "react"
 import { render } from "react-dom"
-import './index.scss'
+import './App.scss'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return <>
-    
+    <Container>
+      
+    </Container>
   </>
 }
 
