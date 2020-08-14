abd130c81d8a4ccc5cbb614d8ef803dad886fa35
Plasmoxy <sebohacker@gmail.com>
2020-08-14 15:19:57 +0200

    i dont even know

--- BartZadanie/pmxy-gallery/src/App.tsx
+++ BartZadanie/pmxy-gallery/src/App.tsx
@@ -7,7 +7,7 @@ import galleryThumb from './images/gallery-thumb.jpg'
 import townJpg from './images/town.jpg'
 import walkercityJpg from './images/walkercity.jpg'
 import nightcityJpg from './images/nightcity.jpg'
-import { Container } from 'react-bootstrap'
+import { Container, Card } from 'react-bootstrap'
 import { Switch } from 'react-router-dom'
 
 
@@ -22,60 +22,26 @@ function App() {
       <h2 className="text-light mt-5">KATEGÓRIE</h2>
       <hr />
       <div className="mt-4"></div>
-
-      <div className="gallery-tiles">
-        <div className="gallery-row">
-          <div className="gallery-column">
-            <div className="gallery-card">
-              <img src={townJpg} />
-              <div className="p-1 pt-2 text-center text-uppercase">
-                <span>Hory</span>
-              </div>
-            </div>
-          </div>
-          <div className="gallery-column">
-            <div className="gallery-card">
-              <img src={walkercityJpg} />
-              <div className="p-1 pt-2 text-center text-uppercase">
-                <span>Mestá</span>
-              </div>
-            </div>
-          </div>
-          <div className="gallery-column">
-            <div className="gallery-card">
-              <img src={galleryThumb} />
-              <div className="p-1 pt-2 text-center text-uppercase">
-                <span>Hory</span>
-              </div>
-            </div>
-          </div>
-          <div className="gallery-column">
-            <div className="gallery-card">
-              <img src={galleryThumb} />
-              <div className="p-1 pt-2 text-center text-uppercase">
-                <span>Hory</span>
-              </div>
-            </div>
-          </div>
-          
-          <div className="gallery-column">
-            <div className="gallery-card">
-              <img src={galleryThumb} />
-              <div className="p-1 pt-2 text-center text-uppercase">
-                <span>Hory</span>
-              </div>
-            </div>
-          </div>
-          <div className="gallery-column">
-            <div className="gallery-card">
-              <img src={galleryThumb} />
-              <div className="p-1 pt-2 text-center text-uppercase">
-                <span>Hory</span>
-              </div>
-            </div>
-          </div>
+      
+      <div className="gallery">
+        <div className="row">
+          {...(() => {
+            const items = [];
+            for (let i = 0; i<= 10; i++) {
+              items.push(
+                <div className="col-sm-6 col-lg-3">
+                  <div className="gallery-card" >
+                    <img src={townJpg} />
+                    <div className="p-1 pt-2 text-center text-uppercase">
+                      <span>Hory</span>
+                    </div>
+                  </div> 
+                </div>
+              )
+            }
+            return items;
+          })()}
         </div>
-        
       </div>
     </div>
   </>
