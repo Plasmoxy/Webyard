3c1f68fce5f1e26a221753cc1a43fbac2a8afb01
Plasmoxy <sebohacker@gmail.com>
2020-08-15 18:22:30 +0200

    cololo

--- BartZadanie/pmxy-gallery/src/App.tsx
+++ BartZadanie/pmxy-gallery/src/App.tsx
@@ -8,7 +8,7 @@ import galleryThumb from './images/gallery-thumb.jpg'
 import townJpg from './images/town.jpg'
 import walkercityJpg from './images/walkercity.jpg'
 import nightcityJpg from './images/nightcity.jpg'
-import { Container, Card, Button } from 'react-bootstrap'
+import { Container, Card, Button, Row, Col } from 'react-bootstrap'
 import { Switch } from 'react-router-dom'
 
 
@@ -19,34 +19,32 @@ function App() {
     </div>
 
     <div className="container content">
+      
       <h1 className="text-light">FOTOGALÉRIA</h1>
       <h2 className="text-light mt-5">KATEGÓRIE</h2>
       <hr />
       <div className="mt-4"></div>
       
       <div className="gallery">
-        <div className="row">
+        <Row>
           {...(() => {
             const items = [];
             for (let i = 0; i<= 10; i++) {
               items.push(
-                <div key={i} className="col-sm-6 col-lg-3">
+                <Col key={i} sm={6} lg={3}>
                   <div className="gallery-card" >
                     <img src={townJpg} />
                     <div className="p-1 pt-2 text-center text-uppercase">
                       <span>Hory</span>
                     </div>
                   </div> 
-                </div>
+                </Col>
               )
             }
             return items;
           })()}
-        </div>
+        </Row>
       </div>
-      
-      
-    
     </div>
   </>
 }
