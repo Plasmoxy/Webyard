b4142ed843bf040225eef938fb7f022e1fe83b44
Plasmoxy <sebohacker@gmail.com>
2020-08-15 21:22:14 +0200

    Update AppModal.tsx

--- BartZadanie/pmxy-gallery/src/components/AppModal.tsx
+++ BartZadanie/pmxy-gallery/src/components/AppModal.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from 'react'
+import React, { ComponentProps, useRef } from 'react'
 import './AppModal.scss'
 
 interface Props {
@@ -9,10 +9,23 @@ interface Props {
 export const AppModal: React.FC<Props> = ({
   visible = false, children, onClose
 }) => {
-  return <div className={`app-modal ${visible ? "d-flex" : ""}`}>
+  
+  const root = useRef<any>()
+  const cls = `app-modal ${visible ? "d-flex" : ""}`
+  
+  const close = () => {
+    if (onClose) onClose()
+  }
+  
+  const click = (e: any) => {
+    // if outside the content
+    if (e.target == root.current) close()
+  }
+  
+  return <div ref={root} className={cls} onClick={click}>
     <div className="app-modal-content">
       <div className="d-flex justify-content-end">
-        <span className="app-modal-close-btn" onClick={onClose}>&times;</span>
+        <span className="app-modal-close-btn" onClick={close}>&times;</span>
       </div>
       {children}
     </div>
