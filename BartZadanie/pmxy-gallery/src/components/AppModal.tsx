2c2f87ab70db5c75d0108ea7da3075b16d962ef9
Plasmoxy <sebohacker@gmail.com>
2020-08-15 23:28:13 +0200

    dd

--- BartZadanie/pmxy-gallery/src/components/AppModal.tsx
+++ BartZadanie/pmxy-gallery/src/components/AppModal.tsx
@@ -14,25 +14,22 @@ export const AppModal: React.FC<Props> = ({
 }) => {
 
   const root = useRef<any>()
-  const cls = `app-modal d-flex`
-
   const close = () => {
     if (onClosed) onClosed()
   }
-
   const click = (e: any) => {
     // if outside the content
     if (e.target == root.current) close()
   }
 
+  
   return <CSSTransition
     in={open}
     timeout={2000}
-    classNames="app-modal-transition"
     unmountOnExit
     onExited={onExited}
   >
-    <div ref={root} className={cls} onClick={click}>
+    <div className="app-modal" ref={root} onClick={click}>
       <div className="app-modal-content">
         <div className="d-flex justify-content-end">
           <span className="app-modal-close-btn" onClick={close}>&times;</span>
