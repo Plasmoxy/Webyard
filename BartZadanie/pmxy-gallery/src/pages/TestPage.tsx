f48bb31320db2e8337ab60409467d143788c027b
Plasmoxy <sebohacker@gmail.com>
2020-08-18 16:49:49 +0200

    Update TestPage.tsx

--- BartZadanie/pmxy-gallery/src/pages/TestPage.tsx
+++ BartZadanie/pmxy-gallery/src/pages/TestPage.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { PageHeader } from '../components/PageHeader'
+import { useHistory } from 'react-router-dom'
 
 export function TestPage() {
 
+  const { location } = useHistory()
+  
+  console.log(location.state)
+  
   return <>
     <PageHeader title="Test" backButton={true} />
     <div className="text-light">
-      TOTO JEST TEST PAGE XDXDXDXD
+      TOTO JEST TEST PAGE XDXDXDXD<br />
     </div>
   </>
 }
