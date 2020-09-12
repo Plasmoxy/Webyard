5a776a2a5588806cbc4dfa5a6a8472c10b513945
Plasmoxy <sebohacker@gmail.com>
2020-09-12 10:46:33 +0200

    hmst

--- CreativeCoding/P5/shader_00_basics/src/sketch.js
+++ CreativeCoding/P5/shader_00_basics/src/sketch.js
@@ -1,6 +1,7 @@
 
 let shaderNames = ["red", "texcoord"]
 let shaders = {}
+let graphiks
 
 function preload() {
   for (let name of shaderNames) {
@@ -10,14 +11,23 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL)
+  graphiks = createGraphics(1000, 1000, WEBGL)
   noStroke()
 }
 
 function draw() {
   background(255)
   
-  shader(shaders['texcoord'])
-  rect(0, 0, width, height)
+  push()
+  graphiks.shader(shaders['texcoord'])
+  graphiks.rect(0, 0, width, height)
+  
+  texture(graphiks)
+  
+  plane(150, 150)
+  translate(100, 100)
+  plane(150, 150)
+  pop()
 }
 
 function windowResized() {
