b5fbd1b2a5e28d8d6c35a7c663c599b427f72cf5
Plasmoxy <sebohacker@gmail.com>
2020-09-12 12:53:06 +0200

    POG

--- _templates/p5js_base_ts/src/sketch.ts
+++ _templates/p5js_base_ts/src/sketch.ts
@@ -2,21 +2,19 @@ import p5 from 'p5'
 
 new p5((p: p5) => {
   
-  let m = p.createVector(2, 2)
   
-  async function t() {
-    console.log("from t")
-  }
-  
-  t()
   
   p.setup = () => {
-    p.createCanvas(p.width, p.height)
+    p.createCanvas(p.windowWidth, p.windowHeight)
     console.log("xd")
   }
   
   p.draw = () => {
-    p.background(255, 0, 255)
+    p.background(0)
+    
   }
+  
+  p.windowResized = () =>
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
   
 })
