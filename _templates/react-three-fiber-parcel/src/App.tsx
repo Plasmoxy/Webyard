8153904be921d8ce2904dbf7d17aefa23c90b6bb
Plasmoxy <sebohacker@gmail.com>
2020-09-13 16:40:58 +0200

    hmm

--- _templates/react-three-fiber-parcel/src/App.tsx
+++ _templates/react-three-fiber-parcel/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.scss'
 import React from "react"
 import { render } from "react-dom"
+import { Canvas } from 'react-three-fiber'
 
 function App() {
   return <>
-    yes
+    <Canvas>
+      <mesh>
+        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+        <meshStandardMaterial attach="material" color="red" />
+      </mesh>
+    </Canvas>
   </>
 }
 
