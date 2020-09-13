b8af63d0eb05918aff8f2da22a890d38b242fcb3
Plasmoxy <sebohacker@gmail.com>
2020-09-13 16:29:58 +0200

    gettin there

---
+++ _templates/react-three-fiber-parcel/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import React from "react"
+import { render } from "react-dom"
+
+function App() {
+  return <>
+    
+  </>
+}
+
+render(
+  <App />,
+  document.getElementById("reactRoot")
+)
