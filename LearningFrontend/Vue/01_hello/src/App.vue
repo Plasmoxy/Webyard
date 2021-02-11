5f91355479bb7d817e668f244402596097336069
Sebastián Petrík <plasmoxy@gmail.com>
2021-02-11 21:02:52 +0100

    pretty fine vue

--- LearningFrontend/Vue/01_hello/src/App.vue
+++ LearningFrontend/Vue/01_hello/src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png"><br/>
-    <counter />
+    <hr />
     
-    <!-- ul -->
+    <counter />
+    <hr/>
+    
+    <!-- strongs ul -->
     <ul>
-      <li v-for="x in strongs" v-bind:key="x">
+      <li v-for="x in strongs" :key="x">
         {{x}}  
       </li>
     </ul>
+    <hr/>
     
+    <!-- reversing -->
     <input type="text" v-model="message" /> <br/>
-    <button v-on:click="reverseMessage">Reverse</button>
+    <button @click="reverseMessage">Reverse</button>
+    <h2>Reversed: {{reversedMsg}}</h2>
+    <hr/>
+    
+    <!-- kolorfule -->
+    <span :style="kolorfulStyleData" @click="increaseKolorful">KOLORFUL</span><br/>
+    <hr/>
+    
   </div>
 </template>
 
 <script>
-import Counter from './components/Counter.vue'
+import Counter from '@/components/Counter.vue'
 
 export default {
   name: 'App',
@@ -28,13 +40,41 @@ export default {
     strongs: [
       "Yes", "No", "mejbi", "yosh", "ananas"
     ],
-    message: ""
+    message: "",
+    kolorfulStyleData: {
+      color: 'red',
+      fontSize: '2rem',
+      cursor: 'pointer',
+      userSelect: 'none',
+    }
   }),
+  
+  computed: {
+    reversedMsg() {
+      return this.message.split('').reverse().join('')
+    },
+    
+    // unused
+    kolorfulStyle() {
+      return {
+        color: 'red',
+        fontSize: '2rem'
+      }
+    }
+  },
   
   methods: {
     reverseMessage() {
-      this.message = this.message.split('').reverse().join('')
+      this.message = this.reversedMsg
+    },
+    
+    increaseKolorful() {
+      this.kolorfulStyleData.fontSize = '' + (parseInt(this.kolorfulStyleData.fontSize)+1) + 'rem'
     }
+  },
+  
+  created() {
+    console.log("App created!")
   }
 }
 </script>
@@ -46,5 +86,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+  
+  margin-bottom: 100px;
+}
+
+hr {
+  margin-bottom: 16px;
+  border: solid 1px rgba(0, 0, 0, 0.1);
 }
 </style>
