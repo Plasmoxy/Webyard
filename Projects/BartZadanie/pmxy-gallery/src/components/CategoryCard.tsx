c735789b372c9ab365edbf28b2269800cd738896
Sebastián Petrík <plasmoxy@gmail.com>
2021-02-12 18:47:14 +0100

    DELETE

--- Projects/BartZadanie/pmxy-gallery/src/components/CategoryCard.tsx
+++ Projects/BartZadanie/pmxy-gallery/src/components/CategoryCard.tsx
@@ -6,14 +6,19 @@ import deletIcon from '../icons/delete.svg'
 interface Props {
   title: string
   image: string
-  onClick?: () => any
+  onClick?: () => any,
+  onDelete?: () => any,
 }
 
 export const CategoryCard: React.FC<Props> = ({
-  title, image, onClick
+  title, image, onClick, onDelete
 }) => {
   return <div className="category-card-wrapper">
-    <img className="deleteIcon" src={deletIcon} />
+    <img className="deleteIcon" src={deletIcon} onClick={e => {
+      if (onDelete) onDelete()
+      e.stopPropagation()
+      e.preventDefault()
+    }} />
     <div className="category-card" onClick={onClick} >
       <img className="photo" src={image} />
       <div className="p-1 pt-2 text-center text-grey">
