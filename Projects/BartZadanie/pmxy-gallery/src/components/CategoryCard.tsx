3e77786b114ee63460a375b9d49f6c41bb26e637
Sebastián Petrík <plasmoxy@gmail.com>
2021-02-12 18:42:19 +0100

    ayayay

--- Projects/BartZadanie/pmxy-gallery/src/components/CategoryCard.tsx
+++ Projects/BartZadanie/pmxy-gallery/src/components/CategoryCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './CategoryCard.scss'
 import galleryThumbJpg from '../images/gallery-thumb.jpg'
+import deletIcon from '../icons/delete.svg'
 
 interface Props {
   title: string
@@ -11,10 +12,13 @@ interface Props {
 export const CategoryCard: React.FC<Props> = ({
   title, image, onClick
 }) => {
-  return <div className="category-card" onClick={onClick} >
-    <img src={image} />
-    <div className="p-1 pt-2 text-center text-grey">
-      <h2>{title}</h2>
+  return <div className="category-card-wrapper">
+    <img className="deleteIcon" src={deletIcon} />
+    <div className="category-card" onClick={onClick} >
+      <img className="photo" src={image} />
+      <div className="p-1 pt-2 text-center text-grey">
+          <h2>{title}</h2>
+      </div>
     </div>
   </div>
 }
