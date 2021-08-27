689dce59fa7dacf576f0d6a690a32e354578bb5a
Plasmoxy <plasmoxy@gmail.com>
2021-08-27 18:00:01 +0200

    fak

--- Projects/soulshrine/src/App.tsx
+++ Projects/soulshrine/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Draggable from 'react-draggable';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+  return <Draggable
+    defaultPosition={{x: -5000, y: -5000}}
+    >
+    <div style={{width: 10000, height: 10000}} className="flex justify-center align-center" >
+      ye
+    </div>
+  </Draggable>
+}
