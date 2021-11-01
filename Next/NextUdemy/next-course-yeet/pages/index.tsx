1909c3a2e09982d3bc11d2a295ecdb7b2751df4b
Plasmoxy <plasmoxy@gmail.com>
2021-11-01 23:21:38 +0100

    tailwindo

---
+++ Next/NextUdemy/next-course-yeet/pages/index.tsx
@@ -0,0 +1,10 @@
+import { InferGetStaticPropsType } from "next"
+import React from "react"
+
+export default function HomePage() {
+  return <div>
+    <span className="text-6xl">
+      Home
+    </span>
+  </div>
+}
