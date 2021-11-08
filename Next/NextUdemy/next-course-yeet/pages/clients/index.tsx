6fac14491398e1d75ddacd5f49092de0c811301e
Plasmoxy <plasmoxy@gmail.com>
2021-11-09 00:46:41 +0100

    ayay

--- Next/NextUdemy/next-course-yeet/pages/clients/index.tsx
+++ Next/NextUdemy/next-course-yeet/pages/clients/index.tsx
@@ -1,13 +1,31 @@
 import React, { ReactElement } from 'react'
+import Link from 'next/link'
 
 interface Props {
   
 }
 
 export default function index({}: Props): ReactElement {
+  
+  const clients = [
+    { id: "max", name: "Max" },
+    { id: "manu", name: "Manu" },
+    { id: "stephanie", name: "Stephanie" },
+    { id: "vicky", name: "Vicky" },
+    { id: "jen ny", name: "Jen ny" },
+  ]
+  
   return (
     <div>
       <h1>Clients page!</h1>
+      <ul>
+        {clients.map(c => <li key={c.id}>
+          <Link href={{
+            pathname: '/clients/[id]',
+            query: { id: c.id }, // this approach auto parses ! very nice
+          }}>{c.name}</Link>
+        </li>)}
+      </ul>
     </div>
   )
 }
